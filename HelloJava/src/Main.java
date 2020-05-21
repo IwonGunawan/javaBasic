3fa9ca6ddfd9642ad7861507b22272ed0d18ec9b
Iwon Gunawan <iwongunawan@gmail.com>
2020-05-21 10:45:24 +0700

    first commit

---
+++ HelloJava/src/Main.java
@@ -0,0 +1,16 @@
+import com.dicoding.javafundamental.basic.kendaraan.Kereta;
+import com.dicoding.javafundamental.basic.kendaraan.Mobil;
+import com.dicoding.javafundamental.basic.kendaraan.Motor;
+import com.dicoding.javafundamental.basic.musik.Gitar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+
+        Gitar.bunyi();
+        Motor.jumlahBan();
+        Mobil.jumlahBan();
+        Kereta.jumlahBan();
+
+    }
+}
